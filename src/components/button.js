c85c551c3acb80c3228fac49ab129d4ffb65d87b
amanymous <aman.mirza9@yahoo.com>
2020-12-14 01:35:53 +0500

    storybook button,text,counter completed moving to next one.

--- src/components/button.js
+++
@@ -1,7 +0,0 @@
-import React from 'react';
-
-export const Button = ({ value, style, className, onClick }) => (
-  <button style={style} className={className} onClick={onClick}>
-    {value}
-  </button>
-);
---
+++ src/components/hello.tsx
@@ -0,0 +1,8 @@
+import React from "react";
+
+const helloworld = () => (
+  <div>
+    <h1>Hello World!</h1>
+  </div>
+);
+export default helloworld;
